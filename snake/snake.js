3d64b10905a7a3842121c6fa1d558011aa329fe6
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-19 19:32:43 -0700

    basic node functionality

--- snake/snake.js
+++ snake/snake.js
@@ -5,26 +5,54 @@ function Snake(startRow, startCol) {
 
 	this.body = [];
 	this.size = 3;
-	this.dir =
+	this.dir = "east";
 
 	// generate snake
 	_(this.size).times(function (n) {
-		that.body[n] = ([startRow, startCol - n]);
+		that.body[n] = ([startRow, startCol + n]);
 	});
-
-	console.log(this.body);
 }
 
 
-Snake.prototype.turn = function () {
+Snake.prototype.turn = function (dir) {
+	if (this.dir === dir) {
+		return;
+	}
+	switch (dir) {
+		case "north":
+			if (this.dir !== "south") {
+				this.dir = dir;
+			}
+			break;
+		case "south":
+			if (this.dir !== "north") {
+				this.dir = dir;
+			}
+			break;
+		case "east":
+			if (this.dir !== "west") {
+				this.dir = dir;
+			}
+			break;
+		case "west":
+			if (this.dir !== "east") {
+				this.dir = dir;
+			}
+			break;
+		default:
+			console.log("Unrecognized direction");
+	}
+
+	this.dir = dir;
 }
 
 function Board(numRows, numCols) {
 	var that = this;
+	this.dimensions = [numCols, numRows];
 	this.snake = new Snake(parseInt(numRows / 2), Math.floor(numCols / 2));
 
 	this.grid = [];
-	this.apples = [[0, 1], [5, 2]];
+	this.apple = [];
 
 	// generate grid
 	_(numRows).times(function (r) {
@@ -34,14 +62,55 @@ function Board(numRows, numCols) {
 		});
 	});
 
+	// place an apple
+	this.generateApple();
+
 }
 
 Board.prototype.step = function () {
-	var headFromTail = this.snake.body.pop();
+	// TODO: only if it isn't an apple
+
+	var head = this.snake.body[0];
+	switch (this.snake.dir) {
+		case "east":
+			this.snake.body.unshift([head[0], head[1] - 1]);
+			break;
+		case "west":
+			this.snake.body.unshift([head[0], head[1] + 1]);
+			break;
+		case "north":
+			this.snake.body.unshift([head[0] - 1, head[1]]);
+			break;
+		case "south":
+			this.snake.body.unshift([head[0] + 1, head[1]]);
+			break;
+		default:
+			console.log("Step failed");
+	}
+	if (this.snake.body[0] !== this.apple) {
+		this.snake.body.pop();
+	} else {
+		this.generateApple();
+	}
+
+	console.log(this.snake.body);
 }
 
-Board.prototype.
+Board.prototype.generateApple = function() {
+	xCoord = Math.floor(Math.random() * this.dimensions[0]);
+	yCoord = Math.floor(Math.random() * this.dimensions[1]);
+	this.apple = [xCoord, yCoord];
+}
 
-board = new Board(10, 20);
-console.log(board.grid[0].length);
+var board = new Board(10, 20);
+console.log(board.apple);
+board.step();
+board.step();
+board.step();
+board.snake.turn("north");
+board.step();
+board.step();
+board.snake.turn("west");
+board.step();
+board.step();
 
